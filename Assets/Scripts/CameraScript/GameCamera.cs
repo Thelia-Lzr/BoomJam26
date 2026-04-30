b2b0e84dc3a32164af4f2abe7e88dcf63a2c4c13
Zhangen <zen53239@gmail.com>
2026-05-01 00:37:36 +0800

    camera
    1

--- Assets/Scripts/CameraScript/GameCamera.cs
+++ Assets/Scripts/CameraScript/GameCamera.cs
@@ -1,23 +1,38 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameCamera : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float moveSpeed = 5f;
+    [Header("摆放框框的摄像机")]
+    public Transform Camera1;
+    [Header("摄像机移动速度")]
+    public float moveSpeed = 3f;
 
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        throw new NotImplementedException();
-    }
+    [Header("摄像机地图边界")]
+    public float minX, maxX;
+    public float minY, maxY;
+    //---------
+    [Header("绑定在小车的摄像机")]
+    public Transform Camera2;
 
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-        rb.velocity = new Vector2(h, v) * moveSpeed;
+        //获取游戏进程，在摆放框框的阶段结束后自动转为绑定在小车上的摄像机
+        if (true)
+        {
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+            Vector3 pos = Camera1.position;
+            pos.x += h * moveSpeed * Time.deltaTime;
+            pos.y += v * moveSpeed * Time.deltaTime;
+            pos.x = Mathf.Clamp(pos.x, minX, maxX);
+            pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+            Camera1.position = pos;
+        }
+        //小车摄像机逻辑
+
     }
 }
