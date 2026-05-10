2b4dd271dafd818b5516f8e2fcc0123c5334c93f
lioluo <midulg5513@gmail.com>
2026-05-10 23:23:42 +0800

    Configure level story flow

--- Assets/Scripts/GagetManager/LevelSelectUI.cs
+++ Assets/Scripts/GagetManager/LevelSelectUI.cs
@@ -50,6 +50,78 @@ public class LevelSelectUI : MonoBehaviour
         }
     }
 
+    public void GoToLevel3PreStory()
+    {
+        if (SceneController.Instance != null)
+        {
+            SceneController.Instance.GoToLevel3PreStory();
+        }
+    }
+
+    public void GoToLevel3PostStory()
+    {
+        if (SceneController.Instance != null)
+        {
+            SceneController.Instance.GoToLevel3PostStory();
+        }
+    }
+
+    public void GoToLevel3Battle()
+    {
+        if (SceneController.Instance != null)
+        {
+            SceneController.Instance.GoToLevel3Battle();
+        }
+    }
+
+    public void GoToLevel4PreStory()
+    {
+        if (SceneController.Instance != null)
+        {
+            SceneController.Instance.GoToLevel4PreStory();
+        }
+    }
+
+    public void GoToLevel4PostStory()
+    {
+        if (SceneController.Instance != null)
+        {
+            SceneController.Instance.GoToLevel4PostStory();
+        }
+    }
+
+    public void GoToLevel4Battle()
+    {
+        if (SceneController.Instance != null)
+        {
+            SceneController.Instance.GoToLevel4Battle();
+        }
+    }
+
+    public void GoToLevel5PreStory()
+    {
+        if (SceneController.Instance != null)
+        {
+            SceneController.Instance.GoToLevel5PreStory();
+        }
+    }
+
+    public void GoToLevel5PostStory()
+    {
+        if (SceneController.Instance != null)
+        {
+            SceneController.Instance.GoToLevel5PostStory();
+        }
+    }
+
+    public void GoToLevel5Battle()
+    {
+        if (SceneController.Instance != null)
+        {
+            SceneController.Instance.GoToLevel5Battle();
+        }
+    }
+
     public void GoToLevelSelect()
     {
         if (SoundManager.SoundManager.Instance != null)
