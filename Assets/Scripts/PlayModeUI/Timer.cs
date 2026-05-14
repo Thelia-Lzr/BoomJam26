e0efa66744d11378eb13b985a4a03f27fa02f6bf
Thelia <teddyyzr@foxmail.com>
2026-05-15 01:15:59 +0800

    addtimer

---
+++ Assets/Scripts/PlayModeUI/Timer.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    TextMeshProUGUI tmp;
+    public static Timer Instance;
+    private float currentTime = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = 0f;
+        tmp = GetComponent<TextMeshProUGUI>();
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        tmp.text = currentTime.ToString("F2")+"s";
+    }
+
+    public void Reset()
+    {
+        currentTime = 0f;
+    }
+}
